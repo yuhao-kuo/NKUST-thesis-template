1cb8f170c69662ce6c6938ec33e848f859771604
yuhao-kuo <st40201@gmail.com>
2021-09-28 13:27:11 +0800

    fix batch bug
    1. fix build.bat not find Dockerfile.
    2. modify docker container status checked.

--- Docker/windows/start.bat
+++ Docker/windows/start.bat
@@ -1,3 +1,14 @@
+@echo off
+
+docker ps | find "latex-srv" > docker.stat
+set /p STAT=<docker.stat
+del "docker.stat"
+
+if ["%STAT"] == [""] (
+	echo "[msg] latex-srv is running!"
+	pause
+	exit
+)
 
 set "BATPATH=%CD%"
 cd %CD%\..\..
